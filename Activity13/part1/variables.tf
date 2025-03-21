c9a42c942f5da1720d5f499fc7e1510a4fd478a7
Jeremy Dougherty <143429873+Jerem-Dough@users.noreply.github.com>
2025-03-21 16:54:35 -0600

    Update variables.tf

--- Activity13/part1/variables.tf
+++ Activity13/part1/variables.tf
@@ -7,12 +7,10 @@ variable "region" {
   default = "us-west-1"
 }
 
-// TODO: update with your default VPC ID
 variable "vpc_id" {
   default = "vpc-0c347a3a6523cbb3e"
 }
 
-// TODO: update with a public subnet id from your default VPC
 variable "subnet_id" {
   default = "subnet-0a521e183660d2e53"
 }
